7affe2770d02d91b6d82b561bfa52ca5ad235a03
Andy Flores <andy.flores@pucp.edu.pe>
2026-05-15 20:19:46 +0200

    adding arguments to launch

--- bringup_devices/bno055_serial_imu/launch/bno055_serial_imu.launch.py
+++ bringup_devices/bno055_serial_imu/launch/bno055_serial_imu.launch.py
@@ -1,18 +1,24 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+
+    port_arg = DeclareLaunchArgument(
+        'port',
+        default_value='/dev/ttyACM0'
+    )
 
     imu_node = Node(
         package='bno055_serial_imu',
         executable='serial_imu_node',
         name='bno055_serial_imu_node',
         output='screen',
-
         parameters=[
             {
-                'port': '/dev/ttyACM0',
+                'port': LaunchConfiguration('port'),
                 'baudrate': 115200,
                 'frame_id': 'imu_link',
                 'topic_name': '/imu/data'
@@ -21,5 +27,6 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        port_arg,
         imu_node
     ])
