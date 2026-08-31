9f7a635aeb6a1c0c7e343018e92ea199251b9e56
Andy Flores <andy.flores@pucp.edu.pe>
2026-08-31 19:15:43 +0200

    checking ring

--- bringup_devices/velodyne_vlp32c_bringup/launch/vlp32c_outdoor.launch.py
+++ bringup_devices/velodyne_vlp32c_bringup/launch/vlp32c_outdoor.launch.py
@@ -139,7 +139,7 @@ def generate_launch_description():
         output='screen',
         parameters=[
             laserscan_params,
-            {'ring': ring}
+            {'ring': 1}
         ],
         remappings=[
             ('scan', 'scan_outdoor_raw')   # unclamped range_max — see scan_range_clamp_node below
